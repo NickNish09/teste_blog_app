ba1a0c56fdbe25990d2d8899e3b6be0cb149652f
Nicholas <nicholasmarques@cjr.org.br>
2018-04-05 13:28:43 -0300

    fixed broken delete spec

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,9 +2,10 @@ require "rails_helper"
 
 RSpec.feature "Listing Articles" do
 	before do
-		@article1 = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consecteur.")
+		@john = User.create(email: "john@example.com",password: "password")
+		@article1 = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consecteur.",user: @john)
 	
-		@article2 = Article.create(title: "The second article", body: "Body of second article")
+		@article2 = Article.create(title: "The second article", body: "Body of second article", user: @john)
 	end
 
 	scenario "A user lists all articles" do
